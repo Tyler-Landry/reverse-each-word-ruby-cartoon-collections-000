e9b35f4095d0ab5134d26a8da517b8d476d2a80a
Tyler Landry <tyler.landry1996@outlook.com>
2019-04-02 00:13:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(words)
- sentence = "Hello there, and how are you?".split(/ /)
- do words.each |x| 
-  put x.reverse
-  end
+  sentence = {"Hello there, and how are you?".split(/ /)}.collection_select
+    do words.each |x| 
+    put x.reverse
+    end
 end
   
   
